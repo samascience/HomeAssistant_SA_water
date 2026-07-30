fd8ad4ea78cfc34dacf22c0054c12293879209d5
Srujan S <srujansama@gmail.com>
2026-07-30 14:52:58 -0500

    feat: add optional SSO authentication

--- custom_components/sensus_analytics_water/const.py
+++ custom_components/sensus_analytics_water/const.py
@@ -5,6 +5,7 @@ DOMAIN = "sensus_analytics_water"
 CONF_BASE_URL = "base_url"
 CONF_USERNAME = "username"
 CONF_PASSWORD = "password"  # nosec
+CONF_SSO_AUTH = "sso_auth"  # nosec
 CONF_ACCOUNT_NUMBER = "account_number"
 CONF_WATER_METER_NUMBER = "water_meter_number"
 
